ead1c7a5deff937a0b35ee5b97b6bab3ae25a557
console-beaver <rybalkin@stanford.edu>
2025-12-11 11:18:49 -0800

    final baseline changes for recording statistics

--- baseline.py
+++ baseline.py
@@ -11,6 +11,7 @@ import pickle
 import os
 from util import load_full_dataset_from_folder, bfs, fetch_dataset, sample_src_dst
 import sys
+import numpy as np
 
 
 def test_random_path():
@@ -56,11 +57,13 @@ def test_random_path():
 
 def bfs_baseline(num_trials, page_names, page_edges):
     name_to_id, id_to_name = page_names
+    out = []
     for t in range(num_trials):
         pos, end, length = sample_src_dst(id_to_name, page_edges, seed=12736712376 + t)
         num_visited = record_num_visits_bfs(page_edges, (pos, end))
         print(f'shortest: {length}, visited: {num_visited}, {id_to_name[pos]} -> {id_to_name[end]}')
-    return None
+        out.append(num_visited)
+    return out
 
 def record_num_visits_bfs(edge_dict, posend):
     pos, end = posend
@@ -73,5 +76,6 @@ if __name__ == '__main__':
     res = fetch_dataset(n)
     if not res: exit()
     name_to_id, id_to_name, edge_dict = res
-    bfs_baseline(20, (name_to_id, id_to_name), edge_dict)
-
+    counts = bfs_baseline(200, (name_to_id, id_to_name), edge_dict)
+    counts = np.asarray(counts)
+    print(f'mean: {np.mean(counts)}, max: {np.max(counts)}, std: {np.std(counts)}')
